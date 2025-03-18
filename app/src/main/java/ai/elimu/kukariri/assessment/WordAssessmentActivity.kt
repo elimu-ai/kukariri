6ece8747bad0c465a0d7a2e8a1e9dd4dfa4b2d36
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-18 11:39:22 +0700

    Convert WordAssessmentActivity to Kotlin. Enable Kotlin for project.

--- app/src/main/java/ai/elimu/kukariri/assessment/WordAssessmentActivity.kt
+++ app/src/main/java/ai/elimu/kukariri/assessment/WordAssessmentActivity.kt
@@ -1,169 +1,177 @@
-package ai.elimu.kukariri.assessment;
+package ai.elimu.kukariri.assessment
 
-import android.animation.ObjectAnimator;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import ai.elimu.analytics.utils.AssessmentEventUtil
+import ai.elimu.analytics.utils.EventProviderUtil
+import ai.elimu.content_provider.utils.ContentProviderUtil.getAllEmojiGsons
+import ai.elimu.content_provider.utils.ContentProviderUtil.getAllWordGsons
+import ai.elimu.kukariri.BuildConfig
+import ai.elimu.kukariri.R
+import ai.elimu.kukariri.logic.ReviewHelper
+import ai.elimu.model.v2.enums.content.WordType
+import ai.elimu.model.v2.gson.content.WordGson
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class WordAssessmentActivity : AppCompatActivity() {
+    private var progressBar: ProgressBar? = null
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+    private var textView: TextView? = null
 
-import ai.elimu.analytics.utils.AssessmentEventUtil;
-import ai.elimu.analytics.utils.EventProviderUtil;
-import ai.elimu.analytics.utils.LearningEventUtil;
-import ai.elimu.content_provider.utils.ContentProviderUtil;
-import ai.elimu.kukariri.BuildConfig;
-import ai.elimu.kukariri.R;
-import ai.elimu.kukariri.logic.ReviewHelper;
-import ai.elimu.model.v2.enums.content.WordType;
-import ai.elimu.model.v2.gson.analytics.WordAssessmentEventGson;
-import ai.elimu.model.v2.gson.analytics.WordLearningEventGson;
-import ai.elimu.model.v2.gson.content.EmojiGson;
-import ai.elimu.model.v2.gson.content.WordGson;
+    private var difficultButton: Button? = null
 
-public class WordAssessmentActivity extends AppCompatActivity {
-
-    private ProgressBar progressBar;
-
-    private TextView textView;
-
-    private Button difficultButton;
-
-    private Button easyButton;
+    private var easyButton: Button? = null
 
     /**
      * The student will iterate through this list of Words until they have all been mastered.
      */
-    private List<WordGson> wordGsonsPendingReview = new ArrayList<>();
+    private val wordGsonsPendingReview: MutableList<WordGson> = ArrayList()
 
     /**
-     * Once a Word has been mastered, it's moved from {@link #wordGsonsPendingReview} to {@link #wordGsonsMastered}.
+     * Once a Word has been mastered, it's moved from [.wordGsonsPendingReview] to [.wordGsonsMastered].
      */
-    private List<WordGson> wordGsonsMastered = new ArrayList<>();
+    private val wordGsonsMastered: MutableList<WordGson> = ArrayList()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.i(getClass().getName(), "onCreate");
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(javaClass.name, "onCreate")
+        super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_word_assessment);
+        setContentView(R.layout.activity_word_assessment)
 
-        progressBar = findViewById(R.id.wordAssessmentProgressBar);
+        progressBar = findViewById(R.id.wordAssessmentProgressBar)
 
-        textView = findViewById(R.id.wordAssessmentTextView);
+        textView = findViewById(R.id.wordAssessmentTextView)
 
-        difficultButton = findViewById(R.id.wordAssessmentDifficultButton);
+        difficultButton = findViewById(R.id.wordAssessmentDifficultButton)
 
-        easyButton = findViewById(R.id.wordAssessmentEasyButton);
+        easyButton = findViewById(R.id.wordAssessmentEasyButton)
 
         // Get a list of the Words that have been previously learned
-        List<WordLearningEventGson> wordLearningEventGsons = EventProviderUtil.getWordLearningEventGsons(getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+        val wordLearningEventGsons = EventProviderUtil.getWordLearningEventGsons(
+            applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+        )
 
         // Get a set of the Words that have been previously learned
-        Set<Long> idsOfWordsInWordLearningEvents = EventProviderUtil.getIdsOfWordsInWordLearningEvents(getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+        val idsOfWordsInWordLearningEvents = EventProviderUtil.getIdsOfWordsInWordLearningEvents(
+            applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+        )
 
         // Get a list of assessment events for the words that have been previously learned
-        List<WordAssessmentEventGson> wordAssessmentEventGsons = EventProviderUtil.getWordAssessmentEventGsons(getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+        val wordAssessmentEventGsons = EventProviderUtil.getWordAssessmentEventGsons(
+            applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+        )
 
         // Determine which of the previously learned Words are pending a review (based on WordAssessmentEvents)
-        Set<Long> idsOfWordsPendingReview = ReviewHelper.getIdsOfWordsPendingReview(idsOfWordsInWordLearningEvents, wordLearningEventGsons, wordAssessmentEventGsons);
-        Log.i(getClass().getName(), "idsOfWordsPendingReview.size(): " + idsOfWordsPendingReview.size());
+        val idsOfWordsPendingReview = ReviewHelper.getIdsOfWordsPendingReview(
+            idsOfWordsInWordLearningEvents,
+            wordLearningEventGsons,
+            wordAssessmentEventGsons
+        )
+        Log.i(javaClass.name, "idsOfWordsPendingReview.size(): " + idsOfWordsPendingReview.size)
 
         // Fetch list of Words from the ContentProvider, and exclude those not in the idsOfWordsPendingReview set
-        List<WordGson> allWordGsons = ContentProviderUtil.INSTANCE.getAllWordGsons(getApplicationContext(), BuildConfig.CONTENT_PROVIDER_APPLICATION_ID);
-        for (WordGson wordGson : allWordGsons) {
-            if (idsOfWordsPendingReview.contains(wordGson.getId())) {
+        val allWordGsons = getAllWordGsons(
+            applicationContext, BuildConfig.CONTENT_PROVIDER_APPLICATION_ID
+        )
+        for (wordGson in allWordGsons) {
+            if (idsOfWordsPendingReview.contains(wordGson.id)) {
                 // Only include adjectives/nouns/verbs
-                if (       (wordGson.getWordType() == WordType.ADJECTIVE)
-                        || (wordGson.getWordType() == WordType.NOUN)
-                        || (wordGson.getWordType() == WordType.VERB)
+                if ((wordGson.wordType == WordType.ADJECTIVE)
+                    || (wordGson.wordType == WordType.NOUN)
+                    || (wordGson.wordType == WordType.VERB)
                 ) {
-                    wordGsonsPendingReview.add(wordGson);
+                    wordGsonsPendingReview.add(wordGson)
                 }
             }
         }
     }
 
-    @Override
-    protected void onStart() {
-        Log.i(getClass().getName(), "onStart");
-        super.onStart();
+    override fun onStart() {
+        Log.i(javaClass.name, "onStart")
+        super.onStart()
 
-        loadNextWord();
+        loadNextWord()
     }
 
-    private void loadNextWord() {
-        Log.i(getClass().getName(), "loadNextWord");
+    private fun loadNextWord() {
+        Log.i(javaClass.name, "loadNextWord")
 
         if (wordGsonsPendingReview.isEmpty()) {
-            Intent intent = new Intent(getApplicationContext(), AssessmentCompletedActivity.class);
-            startActivity(intent);
-            finish();
-            return;
+            val intent = Intent(applicationContext, AssessmentCompletedActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
         }
 
         // Update the progress bar
-        int progressPercentage = wordGsonsMastered.size() * 100 / (wordGsonsPendingReview.size() + wordGsonsMastered.size());
-        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(progressBar, "progress", progressPercentage);
-        objectAnimator.setDuration(1000);
-        objectAnimator.start();
+        val progressPercentage =
+            wordGsonsMastered.size * 100 / (wordGsonsPendingReview.size + wordGsonsMastered.size)
+        val objectAnimator = ObjectAnimator.ofInt(progressBar, "progress", progressPercentage)
+        objectAnimator.setDuration(1000)
+        objectAnimator.start()
 
         // Display the next Word in the list
-        final WordGson wordGson = wordGsonsPendingReview.get(0);
-        textView.setText(wordGson.getText());
-        Animation appearAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_appear_right);
-        textView.startAnimation(appearAnimation);
+        val wordGson = wordGsonsPendingReview[0]
+        textView!!.text = wordGson.text
+        val appearAnimation =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.anim_appear_right)
+        textView!!.startAnimation(appearAnimation)
 
         // Append Emojis (if any) below the Word
-        List<EmojiGson> emojiGsons = ContentProviderUtil.INSTANCE.getAllEmojiGsons(wordGson.getId(), getApplicationContext(), BuildConfig.CONTENT_PROVIDER_APPLICATION_ID);
-        if (!emojiGsons.isEmpty()) {
-            textView.setText(textView.getText() + "\n");
-            for (EmojiGson emojiGson : emojiGsons) {
-                textView.setText(textView.getText() + emojiGson.getGlyph());
+        val emojiGsons = getAllEmojiGsons(
+            wordGson.id,
+            applicationContext, BuildConfig.CONTENT_PROVIDER_APPLICATION_ID
+        )
+        if (emojiGsons.isNotEmpty()) {
+            textView!!.text = textView!!.text.toString() + "\n"
+            for (emojiGson in emojiGsons) {
+                textView!!.text = textView!!.text.toString() + emojiGson.glyph
             }
         }
 
-        final long timeStart = System.currentTimeMillis();
+        val timeStart = System.currentTimeMillis()
 
-        difficultButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(getClass().getName(), "difficultButton onClick");
+        difficultButton!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Log.i(javaClass.name, "difficultButton onClick")
 
                 // Move the Word to the end of the list
-                wordGsonsPendingReview.remove(wordGson);
-                wordGsonsPendingReview.add(wordGson);
+                wordGsonsPendingReview.remove(wordGson)
+                wordGsonsPendingReview.add(wordGson)
 
                 // Report assessment event to the Analytics application (https://github.com/elimu-ai/analytics)
-                AssessmentEventUtil.reportWordAssessmentEvent(wordGson, 0.00f, System.currentTimeMillis() - timeStart, getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+                AssessmentEventUtil.reportWordAssessmentEvent(
+                    wordGson, 0.00f, System.currentTimeMillis() - timeStart,
+                    applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+                )
 
-                loadNextWord();
+                loadNextWord()
             }
-        });
+        })
 
-        easyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(getClass().getName(), "easyButton onClick");
+        easyButton!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Log.i(javaClass.name, "easyButton onClick")
 
                 // Remove the Word from the list of Words to be repeated, and add it to the list of mastered Words
-                wordGsonsPendingReview.remove(wordGson);
-                wordGsonsMastered.add(wordGson);
+                wordGsonsPendingReview.remove(wordGson)
+                wordGsonsMastered.add(wordGson)
 
                 // Report assessment event to the Analytics application (https://github.com/elimu-ai/analytics)
-                AssessmentEventUtil.reportWordAssessmentEvent(wordGson, 1.00f, System.currentTimeMillis() - timeStart, getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+                AssessmentEventUtil.reportWordAssessmentEvent(
+                    wordGson, 1.00f, System.currentTimeMillis() - timeStart,
+                    applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+                )
 
-                loadNextWord();
+                loadNextWord()
             }
-        });
+        })
     }
 }
