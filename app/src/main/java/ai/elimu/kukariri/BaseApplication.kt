ab89ce0bbe68e6106ea8e4be964e7ec77afe598e
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-31 13:45:24 +0700

    Convert BaseApplication to Kotlin

--- app/src/main/java/ai/elimu/kukariri/BaseApplication.kt
+++ app/src/main/java/ai/elimu/kukariri/BaseApplication.kt
@@ -1,22 +1,19 @@
-package ai.elimu.kukariri;
+package ai.elimu.kukariri
 
-import android.app.Application;
-import android.content.Intent;
-import android.util.Log;
-
-import ai.elimu.kukariri.service.ScreenOnService;
-import dagger.hilt.android.HiltAndroidApp;
+import ai.elimu.kukariri.service.ScreenOnService
+import android.app.Application
+import android.content.Intent
+import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-public class BaseApplication extends Application {
-
-    @Override
-    public void onCreate() {
-        Log.i(getClass().getName(), "onCreate");
-        super.onCreate();
+class BaseApplication : Application() {
+    override fun onCreate() {
+        Log.i(javaClass.name, "onCreate")
+        super.onCreate()
 
         // Start service for registering ScreenOnReceiver
-        Intent screenOnServiceIntent = new Intent(getApplicationContext(), ScreenOnService.class);
-        startService(screenOnServiceIntent);
+        val screenOnServiceIntent = Intent(applicationContext, ScreenOnService::class.java)
+        startService(screenOnServiceIntent)
     }
 }
